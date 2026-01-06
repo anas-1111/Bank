1a8c76ec7aefa112ccb6ee6fd8517327da695ef0
Anas_Ibrahem <85556301+anas-1111@users.noreply.github.com>
2026-01-06 16:55:39 +0200

    SQL Files

---
+++ BANKTRUNCATEDWH.sql
@@ -0,0 +1,13 @@
+Truncate Table [DWH].[FactTransactions];
+
+Truncate Table [DWH].[DimState];
+
+Truncate Table [DWH].[DimMCC];
+
+Truncate Table [DWH].[DimErrors];
+
+Truncate Table [DWH].[DimCity];
+
+Truncate Table [DWH].[DimChip];
+
+Truncate Table [DWH].[DimCards];
